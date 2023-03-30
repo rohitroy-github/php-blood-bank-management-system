8a8f2a4ec69a24694dcc4e5adbcd4fb02bc22aac
rohit <rhtry.tech@gmail.com>
2023-03-30 22:47:12 +0530

    add :

---
+++ index.php
@@ -0,0 +1,153 @@
+<!-- Main CMS/ Admin file  -->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="./styles/clientStyles.css">
+    <link rel="stylesheet" href="./styles/index.css">
+
+    <title>Blood Bank | Home</title>
+</head>
+
+<body>
+    <!-- Menu Section -->
+    <div class="top-container">
+        <?php include './partials/navbar.php'; ?>
+    </div>
+
+    <!-- <?php if (isset($_SESSION['login'])) {
+        echo $_SESSION['login'];
+        unset($_SESSION['login']);
+    } ?> -->
+
+    <!-- mainContentSection -->
+    <div class="main-container container" id="dashboard">
+        <div class="content">
+            <h2 style="font-weight: 500; text-align: center;">
+                <b>Blood Bank</b>
+            </h2>
+
+            <div class="table-responsive">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>
+                                <h6><b>Serial</b></h6>
+                            </th>
+                            <th>
+                                <h6><b>Hospital</b></h6>
+                            </th>
+                            <th>
+                                <h6><b>Blood Group</b></h6>
+                            </th>
+                            <th>
+                                <h6><b>Volume</b></h6>
+                            </th>
+                            <th>
+                                <h6><b>Expiry</b></h6>
+                            </th>
+                            <th>
+                                <h6><b>Action</b></h6>
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        $sql = 'SELECT * FROM tbl_order ORDER BY id DESC';
+
+                        $res = mysqli_query($conn, $sql);
+
+                        if ($res == true) {
+                            // Count rows for checking data availibility
+                            $count = mysqli_num_rows($res);
+
+                            $sn = 1;
+
+                            if ($count > 0) {
+                                while ($rows = mysqli_fetch_assoc($res)) {
+
+                                    //Run as long as data is available
+                                    $id = $rows['id'];
+                                    $hospitalName = $rows['hospitalName'];
+                                    $bloodGroup = $rows['bloodGroup'];
+                                    $volume = $rows['volume'];
+                                    $expiryDate = $rows['expiryDate'];
+                                    ?>
+                        <tr>
+                            <td>
+                                <p>
+                                    <?php echo $sn++; ?>
+                                </p>
+                            </td>
+                            <td>
+                                <p>
+                                    <?php echo $hospitalName; ?>
+                                </p>
+                            </td>
+                            <td>
+                                <p>
+                                    <?php echo $bloodGroup; ?>
+                                </p>
+                            </td>
+                            <!-- <td>
+                                <p>
+                                <?php echo $volume; ?>
+                                </p>
+                            </td> -->
+                            <td>
+                                <p>
+                                    <?php echo $expiryDate; ?>
+                                </p>
+                            </td>
+                            <td>
+                                <div class="d-flex justify-content-center">
+                                    <a href="<?php echo HOMEURL; ?>admin/update-order.php?id=<?php echo $id; ?>"
+                                        class="btn adminPanelBtn mr-2">
+                                        Request Blood
+                                    </a>
+                                </div>
+                            </td>
+                        </tr>
+
+                        <?php
+                                }
+                            } else {
+                                 ?>
+                        <tr>
+                            <td>
+                                <p>
+                                    Currently there are no orders !
+                                </p>
+                            </td>
+                        </tr>
+                        <?php
+                            }
+                        }
+                        ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+            
+            </div>
+        </div>
+    </div>
+
+    <!-- footerSection -->
+    <div class="bottom-container">
+    <?php include './partials/footer.php'; ?>
+    </div>
+
+    <!-- Bootstrap JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.9.3/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+
+</html>
